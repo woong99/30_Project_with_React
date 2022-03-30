27c1e417a13a2f32296adc1f37aabbd4635ce60b
woong99 <dndry1072@naver.com>
2022-03-30 14:53:35 +0900

    virtual keyboard

---
+++ virtual_keyboard/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  optimization: {
+    minimizer: [new TerserWebpackPlugin()],
+  },
+};
