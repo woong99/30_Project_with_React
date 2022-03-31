32ba15440565004142e1ec1cb93cba267cc6d2f0
woong99 <dndry1072@naver.com>
2022-03-31 21:11:10 +0900

    virtual-keyboard

--- virtual_keyboard/src/js/keyboard.js
+++ virtual_keyboard/src/js/keyboard.js
@@ -1,11 +1,14 @@
 export class Keyboard {
   #swichEl;
+  #fontSelectEl;
+
   constructor() {
     this.#assignElement();
     this.#addEvent();
   }
   #assignElement() {
     this.#swichEl = document.querySelector("#switch");
+    this.#fontSelectEl = document.querySelector("#font");
   }
 
   #addEvent() {
@@ -14,6 +17,9 @@ export class Keyboard {
         "theme",
         event.target.checked ? "dark-mode" : ""
       );
+    });
+    this.#fontSelectEl.addEventListener("change", (event) => {
+      document.body.style.fontFamily = event.target.value;
     });
   }
 }
