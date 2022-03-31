a3efb515060af6ced168d178a3bad59b45f9aa20
woong99 <dndry1072@naver.com>
2022-03-31 21:38:41 +0900

    virtual-keyboard

--- virtual_keyboard/src/js/keyboard.js
+++ virtual_keyboard/src/js/keyboard.js
@@ -1,25 +1,48 @@
 export class Keyboard {
   #swichEl;
   #fontSelectEl;
+  #containerEl;
+  #keyboardEl;
+  #inputEl;
 
   constructor() {
     this.#assignElement();
     this.#addEvent();
   }
   #assignElement() {
-    this.#swichEl = document.querySelector("#switch");
-    this.#fontSelectEl = document.querySelector("#font");
+    this.#containerEl = document.querySelector("#container");
+    this.#swichEl = this.#containerEl.querySelector("#switch");
+    this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputEl = this.#containerEl.querySelector("#input");
   }
 
   #addEvent() {
-    this.#swichEl.addEventListener("change", (event) => {
-      document.documentElement.setAttribute(
-        "theme",
-        event.target.checked ? "dark-mode" : ""
-      );
+    this.#swichEl.addEventListener("change", this.#onChangeTheme);
+    this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", (event) => {
+      this.#keyboardEl
+        .querySelector(`[data-code=${event.code}]`)
+        ?.classList.add("active");
+
+      this.#inputEl.value += event.key;
     });
-    this.#fontSelectEl.addEventListener("change", (event) => {
-      document.body.style.fontFamily = event.target.value;
+
+    document.addEventListener("keyup", (event) => {
+      this.#keyboardEl
+        .querySelector(`[data-code=${event.code}]`)
+        ?.classList.remove("active");
     });
+  }
+
+  #onChangeTheme(event) {
+    document.documentElement.setAttribute(
+      "theme",
+      event.target.checked ? "dark-mode" : ""
+    );
+  }
+
+  #onChangeFont(event) {
+    document.body.style.fontFamily = event.target.value;
   }
 }
