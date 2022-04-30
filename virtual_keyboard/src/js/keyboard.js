d9d05aec7a2fdebcb6c239a3725bede8621866fc
woong99 <dndry1072@naver.com>
2022-04-30 18:54:19 +0900

    virtual-keyboard

--- virtual_keyboard/src/js/keyboard.js
+++ virtual_keyboard/src/js/keyboard.js
@@ -1,8 +1,11 @@
 export class Keyboard {
+  // #변수 => private class fields
+  // 해당 필드를 선언한 class에서만 접근이 가능
   #swichEl;
   #fontSelectEl;
   #containerEl;
   #keyboardEl;
+  #inputGroupEl;
   #inputEl;
 
   constructor() {
@@ -14,25 +17,19 @@ export class Keyboard {
     this.#swichEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
     this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
-    this.#inputEl = this.#containerEl.querySelector("#input");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
+    this.#inputEl = this.#inputGroupEl.querySelector("#input");
   }
 
   #addEvent() {
-    this.#swichEl.addEventListener("change", this.#onChangeTheme);
-    this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
-    document.addEventListener("keydown", (event) => {
-      this.#keyboardEl
-        .querySelector(`[data-code=${event.code}]`)
-        ?.classList.add("active");
+    this.#swichEl.addEventListener("change", this.#onChangeTheme); // 테마 변경
+    this.#fontSelectEl.addEventListener("change", this.#onChangeFont); // 폰트 변경
 
-      this.#inputEl.value += event.key;
-    });
-
-    document.addEventListener("keyup", (event) => {
-      this.#keyboardEl
-        .querySelector(`[data-code=${event.code}]`)
-        ?.classList.remove("active");
-    });
+    // 키보드를 눌렀을 때
+    document.addEventListener("keydown", this.#onKeyDown.bind(this));
+    // 키보드에서 땠을 떄
+    document.addEventListener("keyup", this.#onKeyUp.bind(this));
+    this.#inputEl.addEventListener("input", this.#onInput);
   }
 
   #onChangeTheme(event) {
@@ -44,5 +41,26 @@ export class Keyboard {
 
   #onChangeFont(event) {
     document.body.style.fontFamily = event.target.value;
+  }
+
+  #onKeyUp(event) {
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.remove("active");
+  }
+
+  #onKeyDown(event) {
+    this.#inputGroupEl.classList.toggle(
+      "error",
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key)
+    );
+
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.add("active");
+  }
+
+  #onInput(event) {
+    event.target.value = event.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
   }
 }
